00d7e4625ae6ec0316514c368b0ae34692cabced
Carl Hamilton <carlanthonyhamilton@gmail.com>
2020-11-01 14:50:06 +0000

    Extend "stylelint-config-css-modules"

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,33 +4,33 @@
 
 const stylelint = require('stylelint');
 
-const config = require('../index');
+const configuration = require('../index');
 
 describe('configuration', () => {
-  it('should not report an error (correctly ordered properties)', async () => {
-    const code = 'a {\n  top: 0;\n  color: red;\n}\n';
+  const code = `
+    a {
+      top: 0;
+      color: red;
+    }
+  `;
 
+  it('should not report errors', async () => {
     const { errored } = await stylelint.lint({
       code,
-      config,
+      config: {
+        rules: [],
+      },
     });
 
     expect(errored).toBe(false);
   });
 
-  it('should report an error (incorrectly ordered properties)', async () => {
-    const code = 'a {\n  color: red;\n  top: 0;\n}\n';
-
-    const { errored, results } = await stylelint.lint({
+  it('should report errors', async () => {
+    const { errored } = await stylelint.lint({
       code,
-      config,
+      config: configuration,
     });
 
-    const { warnings } = results[0];
-
     expect(errored).toBe(true);
-    expect(warnings).toHaveLength(1);
-    expect(warnings[0].rule).toEqual('order/properties-order');
-    expect(warnings[0].text).toContain('Expected "top" to come before "color"');
   });
 });
--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 module.exports = {
   extends: [
     'stylelint-config-standard',
+    'stylelint-config-css-modules',
     'stylelint-config-property-sort-order-smacss',
   ],
 };
